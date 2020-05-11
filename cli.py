37459772206e3b6ad93bbe954505dafbbfd1cedd
Andrew Hall <hndrewaall@gmail.com>
2020-05-10 17:59:42 -0700

    Add get-user

--- cli.py
+++ cli.py
@@ -1,6 +1,15 @@
+#! /usr/bin/env python
+
+import os
+from pprint import pprint
 import sys
+from urllib.parse import urljoin
 
 import click
+import requests
+
+BASE = os.getenv("EMBY_API_URL")
+KEY = os.getenv("EMBY_API_KEY")
 
 
 @click.group()
@@ -8,10 +17,16 @@ def cli():
     pass
 
 
-@cli.command("get-policy")
-@click.argument("user", type=str)
-def get_policy(user: str):
-    pass
+@cli.command("get-user")
+@click.argument("username", type=str)
+def get_user(username: str):
+    users_resp = requests.get(urljoin(BASE, "/Users"), params={"api_key": KEY})
+    if users_resp.status_code == 200:
+        users = {user["Name"]: user for user in users_resp.json()}
+
+        pprint(users[username])
+    else:
+        print("Invalid response code received: {users_resp.status_code}")
 
 
 if __name__ == "__main__":
